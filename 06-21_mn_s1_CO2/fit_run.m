31f2bdb9bbf7dce53511f3e5f142eff64d53203c
Howard E. Motteler <motteler@gmail.com>
2021-06-23 13:46:57 -0400

    snapshot after mn side 1 CO2 analysis

---
+++ 06-21_mn_s1_CO2/fit_run.m
@@ -0,0 +1,146 @@
+%
+% fit_run -- set up params, call fit_tran, plot results
+%
+% updated to get met laser from neon
+% set test dir, band, optional subsetting and gas below
+%
+
+%----------------
+% paths and libs
+%----------------
+
+addpath /asl/packages/ccast/source
+addpath /asl/packages/ccast/motmsc/utils
+addpath /asl/packages/airs_decon/test
+addpath ../source
+addpath ../gas_calcs
+
+%------------------
+% basic run params
+%------------------
+
+band = 'LW';      % set the band
+test_dir = '.';   % location of test data
+sdir = 0;         % sweep direction
+
+% get wlaser from eng and neon
+opt2 = struct;
+opt2.neonWL = 703.44765;
+load(fullfile(test_dir, 'FT2'));
+[wlaser, wtime] = metlaser(d1.packet.NeonCal, opt2);
+
+fprintf(1, 'eng neon=%.5f assigned neon=%.5f, wlaser=%.5f\n', ... 
+  d1.packet.NeonCal.NeonGasWavelength, opt2.neonWL, wlaser);
+
+% set the search grid
+wgrid = -0.03 : .0005 : 0.03;
+waxis = wlaser + wgrid;
+
+% run name for plots
+pname = 'CO2, 21 Jun 2021 MN side 1';
+
+%---------------------
+% params for fit_tran
+%---------------------
+
+opt = struct; 
+opt.user_res = 'hires';
+opt.inst_res = 'hires4';
+opt.LW_sfile = '../inst_data/SAinv_default_HR4_LW.mat';
+% opt.qv1 = 650; opt.qv2 = 760;  % full fitting interval 
+  opt.qv1 = 672; opt.qv2 = 712;  % easier CO2 subinterval
+% opt.qv1 = 676; opt.qv2 = 712;  % Larrabee new tests
+
+% gas file and weight
+opt.abswt = 12.69;
+opt.afile = 'lblr_CO2_49p54_Torr_16p80_C';
+
+%--------------------
+% get interferograms
+%--------------------
+
+% test data files
+mat_et2 = fullfile(test_dir, 'ET2');
+mat_et1 = fullfile(test_dir, 'ET1');
+mat_ft2 = fullfile(test_dir, 'FT2');
+mat_ft1 = fullfile(test_dir, 'FT1');
+
+% load the interferograms
+igm = struct;
+igm.ET2 = read_igm(band, mat_et2, sdir);
+igm.ET1 = read_igm(band, mat_et1, sdir);
+igm.FT2 = read_igm(band, mat_ft2, sdir);
+igm.FT1 = read_igm(band, mat_ft1, sdir);
+
+% option to take subsets
+igm.ET2 = igm.ET2(:, :, 20:345);
+igm.ET1 = igm.ET1(:, :, 594:914);
+igm.FT2 = igm.FT2(:, :, 25:340);
+igm.FT1 = igm.FT1(:, :, 25:345);
+
+%---------------
+% call fit_tran
+%---------------
+
+[drms, fmsc] = fit_tran(band, waxis, igm, opt);
+wa = fmsc.wa;          % "a" weights, for each FOV
+wb = fmsc.wb;          % "b" weights, for each FOV
+dmin = fmsc.dmin;      % best fit residual
+wfov = fmsc.wfov;      % best fit wlaser
+vobs4 = fmsc.vobs4;    % interpolated frequency grid
+tcal4 = fmsc.tcal4;    % interpolated calc for all FOVS
+tobs4 = fmsc.tobs4;    % interpolated best-fit obs
+tobs5 = fmsc.tobs5;    % interpolated corrected obs
+
+% print fit_tran residuals and weights
+fit_info(band, wlaser, waxis, drms, fmsc)
+
+% values for plots
+qv1 = opt.qv1;
+qv2 = opt.qv2;
+
+% -----------------------------------
+% plot fitting errors by wlaser shift
+% -----------------------------------
+
+figure(1); clf;
+set(gcf, 'DefaultAxesColorOrder', fovcolors);
+plot(waxis, drms, 'linewidth', 2)
+xlim([round2n(waxis(1), 5), round2n(waxis(end), 5)])
+xlabel('wavelength, nm')
+ylabel('rms fitting error')
+title(sprintf('%s, residual as a function of wlaser', pname));
+legend(fovnames, 'location', 'north')
+grid on; zoom on
+saveas(gcf, 'CO2_wlaser_fit', 'fig')
+
+% ------------------
+% plot obs and calc
+% ------------------
+
+figure(2); clf
+set(gcf, 'DefaultAxesColorOrder', fovcolors);
+plot(vobs4, tobs5, vobs4, tcal4, 'k-.');
+xlim([qv1, qv2])
+xlabel('wavenumber')
+ylabel('transmittance')
+title(sprintf('%s, obs and calc transmittance', pname));
+legend(fovnames, 'location', 'southeast')
+grid on; zoom on
+saveas(gcf, 'CO2_obs_and_calc', 'fig')
+
+% --------------------
+% plot obs minus calc
+% --------------------
+
+figure(3); clf
+set(gcf, 'DefaultAxesColorOrder', fovcolors);
+plot(vobs4, tobs5 - tcal4);
+xlim([qv1, qv2])
+xlabel('wavenumber')
+ylabel('transmittance difference')
+title(sprintf('%s, obs minus calc', pname));
+legend(fovnames, 'location', 'southeast')
+grid on; zoom on
+
+
---
+++ source/check_leg.m
@@ -0,0 +1,168 @@
+%
+% check_leg -- quick look at a test leg
+%
+% main test parameters
+%   band    - 'LW', 'MW', or 'SW'
+%   wlaser  - metrology laser half-wavelength
+%   mfile   - mat file, output from the MIT reader
+%   sdir    - sweep direction, 0 or 1
+%   ifrq    - frequency index for figure 2
+%   ifov    - choose a FOV for figure 3
+%
+% derived from spec_test1
+%
+
+% paths and libs
+addpath /asl/packages/ccast/source
+addpath /asl/packages/ccast/motmsc/time
+addpath ../source
+
+% select a band
+band = upper(input('band (e.g., LW) > ', 's'));
+
+% select a test leg
+tleg = upper(input('test leg (e.g., FT2) > ', 's'));
+mfile = fullfile('./', tleg);
+load(mfile);
+
+% harvest directory
+harvest = 'harvest_mn';
+
+% get wlaser from eng and neon
+opt2 = struct;
+opt2.neonWL = 703.44765;  % Larrabee's value
+[wlaser, wtime] = metlaser(d1.packet.NeonCal, opt2);
+
+% get instrument params
+opt1 = struct; 
+opt1.user_res = 'hires';
+opt1.inst_res = 'hires4';
+[inst, user] = inst_params(band, wlaser, opt1);
+
+% choose a sweep direction
+sdir = 0;
+
+fprintf(1, 'eng neon=%.5f assigned neon=%.5f, wlaser=%.5f\n', ... 
+  d1.packet.NeonCal.NeonGasWavelength, opt2.neonWL, wlaser);
+
+% break out the igm data
+[igm_es, time_es, igm_sp, time_sp, igm_it, time_it] = ...
+   igm_breakout(band, d1, sdir);
+
+% translate to count spectra
+spec_es = igm2spec(igm_es, inst);
+spec_sp = igm2spec(igm_sp, inst);
+spec_it = igm2spec(igm_it, inst);
+
+% get obs times in matlab format
+dnum_es = iet2dnum(time_es(5,:));
+dtime_es = datetime(dnum_es, 'ConvertFrom', 'datenum');
+
+dnum_sp = iet2dnum(time_sp(5,:));
+dtime_sp = datetime(dnum_sp, 'ConvertFrom', 'datenum');
+
+dnum_it = iet2dnum(time_it(5,:));
+dtime_it = datetime(dnum_it, 'ConvertFrom', 'datenum');
+
+% test start and end times
+t1 = dtime_es(1);
+t2 = dtime_es(end);
+
+% get the associated ccs data
+fmt = '../%s/ccs_data_%02d_%02d';
+d2 = load(sprintf(fmt, harvest, t1.Month, t1.Day));
+
+% show all FOVs at one frequency
+figure(1); clf
+set(gcf, 'DefaultAxesColorOrder', fovcolors);
+ifrq = floor(inst.npts/2);
+vseq = squeeze(spec_es(ifrq, :, :));
+[m, nobs] = size(vseq);
+plot(dtime_es, abs(vseq), '.')
+xlim([t1, t2])
+title(sprintf('test %s, all FOVs at %.2f cm-1', tleg, inst.freq(ifrq)))
+legend(fovnames,  'location', 'best')
+xlabel('time')
+ylabel('count')
+grid on; zoom on
+
+% ES time span cache
+ind_file = sprintf('%s_ind.mat', tleg);
+if exist(ind_file) == 2 
+
+  % use exsiting time spans
+  d3 = load(ind_file);
+  t1 = d3.t1;
+  t2 = d3.t2;
+  fprintf(1, 't1 = %s\n', t1)
+  fprintf(1, 't2 = %s\n', t2)
+  input('continue with cached times > ')
+  xlim([t1, t2])
+
+else
+  % chose a test subset
+  tx = input('t1 (hh:mm:ss) > ', 's');
+  [~,~,~, t1.Hour, t1.Minute, t1.Second] = datevec(tx);
+  xlim([t1, t2])
+
+  tx = input('t2 (hh:mm:ss) > ', 's');
+  [~,~,~, t2.Hour, t2.Minute, t2.Second] = datevec(tx);
+  xlim([t1, t2])
+
+  save(ind_file, 't1', 't2');
+end
+
+% show the subset intervals
+j1 = find(dtime_es >= t1, 1);
+j2 = find(dtime_es >= t2, 1);
+if isempty(j2), j2 = nobs; end
+fprintf('%s index = %d:%d\n', tleg, j1, j2)
+
+% show ccs test parameters
+figure(2); clf
+subplot(3,1,1)
+plot(d2.htbb_temp_a_date, d2.htbb_temp_a_val, ...
+     d2.htbb_temp_b_date, d2.htbb_temp_b_val, 'linewidth', 2)
+xlim([t1, t2])
+% ylim([310, 350]);
+title(sprintf('test %s, HTBB Temperatures', tleg))
+% legend('A', 'B', 'location', 'southwest')
+  legend('A', 'B', 'location', 'northeast')
+ylabel('degrees (K)')
+grid on
+
+subplot(3,1,2)
+plot(d2.inficon_press_date, d2.inficon_press_val, 'linewidth', 2)
+xlim([t1, t2])
+% ylim([0, 110]);
+title('gas cell pressure')
+ylabel('Torr')
+grid on
+
+subplot(3,1,3)
+plot(d2.gas_cell_temp_date, d2.gas_cell_temp_val)
+xlim([t1, t2])
+% ylim([14.6, 15.4]);
+title('gas cell temperature')
+ylabel('degrees (C)')
+xlabel('time')
+grid on
+
+% return
+
+% show all obs for one FOV
+figure(3);
+ifov = 5;
+plot(inst.freq, squeeze(abs(spec_es(:, ifov, j1:j2))), 'b');
+title(sprintf('test %s, FOV %d all obs', tleg, ifov))
+xlabel('wavenumber')
+ylabel('count')
+grid on; zoom on
+
+% truncate t1 and t2 to spanning minutes, for plots
+% t1.Second = 0;
+% if t2.Second ~= 0
+%   t2.Second = 0;
+%   t2.Minute = t2.Minute + 1;
+% end
+
